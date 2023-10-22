9492930a4e36edcd87cb675016e9a45273127eb2
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-10-21 19:26:55 -0700

    exercise 11.20 - test: adds another cypress test

--- cypress/e2e/pipeline.cy.js
+++ cypress/e2e/pipeline.cy.js
@@ -4,4 +4,9 @@ describe('Boilerplate', function() {
     cy.contains('Welcome')
     cy.contains('Messages')
   })
+  it('can navigate from main page to messages', function() {
+    cy.visit('http://localhost:8000')
+    cy.get('a[href*="messages"]').click()
+    cy.contains('Send!')
+  })
 })
